6ed5c311d7e583c69b49bebbe0a042cd0f4aafe2
S1a0m <remveille@gmail.com>
2023-11-05 12:30:19 +0100

    mise en place de la methode de connection a la BDD racine

--- RacineRegister.java
+++ RacineRegister.java
@@ -1,16 +1,33 @@
 package racine;
 
+import java.sql.*;
+
 public class RacineRegister {
 	
 	private String appId, userId, password;   // app name or website name
 											  // user name, email or phone number
 	
-	public RacineRegister(String pappId, String puserId, String ppassword) {
+	public RacineRegister(String appId, String userId, String password) {
 		// constructor definition
-		appId = pappId;
-		userId = puserId;
-		password = ppassword;
-		this.make_key();
+		this.appId = appId;
+		this.userId = userId;
+		this.password = password;
+	}
+	
+	static boolean connect_db() {
+		// creer a chaque fois la BDD s'il elle n'existe pas
+		// renvoi vrai quand ouverture possible faux a contrariot
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connect = DriverManager.getConnection("jdbc:sqlite:racine.db");
+			return true;
+		}
+		catch (Exception e) {
+			// System.out.println("Cannot connect to racine.db");
+			// System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			// System.exit(0);
+			return false;
+		}
 	}
 	
 	public boolean register_user() {
