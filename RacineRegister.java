df4328b76de4e3feeff2863516d0ac87bd26c4fc
S1a0m <remveille@gmail.com>
2023-11-06 13:49:01 +0100

    ajout des methodes d'enregistrements d'elements et de creation de table. Suppression de la methode d'essai de connection

--- RacineRegister.java
+++ RacineRegister.java
@@ -6,6 +6,8 @@ public class RacineRegister {
 	
 	private String appId, userId, password;   // app name or website name
 											  // user name, email or phone number
+	private static Connection connect = null;
+	private static Statement state = null;
 	
 	public RacineRegister(String appId, String userId, String password) {
 		// constructor definition
@@ -14,28 +16,76 @@ public class RacineRegister {
 		this.password = password;
 	}
 	
-	static boolean connect_db() {
+	/*static boolean connect_r() {
 		// creer a chaque fois la BDD s'il elle n'existe pas
 		// renvoi vrai quand ouverture possible faux a contrariot
 		try {
 			Class.forName("org.sqlite.JDBC");
-			Connection connect = DriverManager.getConnection("jdbc:sqlite:racine.db");
+			connect = DriverManager.getConnection("jdbc:sqlite:racine.db");
 			return true;
 		}
 		catch (Exception e) {
 			// System.out.println("Cannot connect to racine.db");
-			// System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			// System.exit(0);
+			return false;
+		}
+	}*/
+	
+	static boolean table_r() {
+		// create racine table if not exists
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connect = DriverManager.getConnection("jdbc:sqlite:racine.db");
+			state = connect.createStatement();
+			
+			String query = "CREATE TABLE Racine"
+					+ "(id UNSIGNED NOT NULL AUTO_INCREMENT,"
+					+ "userId VARCHAR(80) NOT NULL,"
+					+ "appId VARCHAR(80) NOT NULL,"
+					+ "password TEXT NOT NULL,"
+					+ "key TEXT NOT NULL,"
+					+ "PRIMARY KEY (id)";
+			state.executeUpdate(query);
+			
+			state.close();
+			connect.close();
+			return true;
+		}
+		catch (Exception e) {
+			// System.out.println("Cannot connect to racine.db");
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			// System.exit(0);
 			return false;
 		}
 	}
 	
-	public boolean register_user() {
+	public boolean register_r() {
 		// register user data in a data base
-		return true;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connect = DriverManager.getConnection("jdbc:sqlite:racine.db");
+			connect.setAutoCommit(false);
+			state = connect.createStatement();
+			
+			String query = "INSERT INTO Racine (userId, appId, password)"
+					+ "VALUES (" + this.appId + "," + this.appId + "," 
+					+ this.password + this.key_r() + ");";
+			state.executeUpdate(query);
+			
+			state.close();
+			connect.commit();
+			connect.close();
+			return true;
+		}
+		catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			// System.exit(0);
+			return false;
+		}
 	}
 	
-	public String make_key() {
+	public String key_r() {
 		/* create unique key about
 		 * user information
 		 */
